39ae4e26db0272b99af034a9f4fb1d45daad56e3
joseenrique61 <joseenrique61@hotmail.com>
2023-06-19 15:40:17 -0500

    Se añade la lógica para recolección e impresión de datos

--- ejercicio.c
+++ ejercicio.c
@@ -12,4 +12,37 @@ int main()
     };
 
     struct alumno alumnos[5];
+
+    for (int i = 0; i < 5; i++){
+        printf("Ingrese los datos del alumno %i\n", i + 1);
+
+        fflush(stdin);
+        printf("Ingrese el nombre: ");
+        gets(alumnos[i].nombre);
+
+        printf("Ingrese la direccion: ");
+        gets(alumnos[i].direccion);
+
+        printf("Ingrese la carrera: ");
+        gets(alumnos[i].carrera);
+
+        fflush(stdin);
+        printf("Ingrese la edad: ");
+        scanf("%i", &alumnos[i].edad);
+
+        fflush(stdin);
+        printf("Ingrese el promedio: ");
+        scanf("%f", &alumnos[i].promedio);
+    }
+
+    for (int i = 0; i < 5; i++){
+        printf("Alumno %i:\n", i + 1);
+        puts("Nombre: ");
+        puts(alumnos[i].nombre);
+        puts("\nDireccion: ");
+        puts(alumnos[i].direccion);
+        puts("\nCarrera: ");
+        puts(alumnos[i].carrera);
+        printf("\nEdad: %i\nPromedio: %f\n\n", alumnos[i].edad, alumnos[i].promedio);
+    }
 }
